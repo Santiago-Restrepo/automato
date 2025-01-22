a59b4bd56ffee7a0252691da8e61e4c37670a459
Santiago Restrepo <srestrepo@abatechnology.com>
2025-01-22 16:56:48 -0500

    feat: implement swagger

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import configuration from './config/configuration';
+import configuration, { Config, ServerConfig } from './config/configuration';
 import { SessionBuilder } from '@ngrok/ngrok';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import setupSwagger from './config/swagger.config';
+import { ConfigService } from '@nestjs/config';
+import setupPipes from './config/global-pipes.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-  await app.listen(configuration().port);
-
-  if (process.env.NGROK_AUTHTOKEN) {
-    // Setup ngrok ingress
-    const session = await new SessionBuilder().authtokenFromEnv().connect();
-    const listener = await session.httpEndpoint().listen();
-    new Logger('main').log(`Ingress established at ${listener.url()}`);
-    listener.forward(`localhost:${configuration().port}`);
-  }
+  const configService = app.get<ConfigService<Config, true>>(ConfigService);
+  const server: ServerConfig = configService.get('server');
+  setupPipes(app);
+  setupSwagger(app);
+  await app.listen(server.port).then(() => {
+    Logger.verbose(`Server running on port ${server.port}`);
+    Logger.verbose(
+      `Swagger docs running on http://localhost:${server.port}/docs`,
+    );
+  });
 }
 bootstrap();
